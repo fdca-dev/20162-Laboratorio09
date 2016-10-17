7dd2886d2f8442126cf48733a9e397b197f571d6
Fabrício <fabricio.ayres@gmail.com>
2016-10-17 02:05:04 -0200

    Laboratório 09

---
+++ app/src/main/java/com/example/fabricio/laboratorio09/PrincipalActivity.java
@@ -0,0 +1,20 @@
+package com.example.fabricio.laboratorio09;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+
+
+
+
+
+    }
+}
